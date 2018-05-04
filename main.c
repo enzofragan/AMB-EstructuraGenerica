de5447061ece68c338b397144c831b0cbe93174f
enzo <36193661+enzofragan@users.noreply.github.com>
2018-05-03 23:14:57 -0300

    logre pedir datos y guardarlo pero error en inicio

--- main.c
+++ main.c
@@ -1,31 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "EstructuraGenerica.h"
-//#include "susFunciones.h"
-#define CANTIDAD 10
+#include <string.h>
+#include "lib.h"
+#define CantUsuarios 10
+#define CantProductos 10
+
 int main()
 {
-    eGenerica listadoGenerico[CANTIDAD];
-    eGen_init(listadoGenerico,CANTIDAD);
+    eUsuario listadoDeUsuario[CantUsuarios];
+    int opcion;
+    int i;
+    char seguir = 's';
 
-    printf( "\n \t muestro el array... esta vacio");
-    eGen_mostrarListado(listadoGenerico,CANTIDAD);
-    getchar();
+    while (seguir=='s')
+    {
 
-    printf( "\n \t muestro el array con los borrados y vacios");
-    eGen_mostrarListadoConBorrados(listadoGenerico,CANTIDAD);
-    getchar();
+     printf("1. alta\n2. baja\n3. modificacion\n4.mostrar\nElija una opcion: ");
+     scanf("%d",&opcion);
 
-    eGen_alta(listadoGenerico,CANTIDAD);
-    eGen_alta(listadoGenerico,CANTIDAD);
-    printf( "\n \t cargo dos elementos y muestro el array");
-    eGen_mostrarListado(listadoGenerico,CANTIDAD);
-    getchar();
+     eUso_inicio(listadoDeUsuario,CantUsuarios);
 
-    printf( "\n \t muestro el array con los borrados y vacios");
-    eGen_mostrarListadoConBorrados(listadoGenerico,CANTIDAD);
-    getchar();
+     switch(opcion)
+        {
+        case 1:
+        eUso_alta(listadoDeUsuario,CantUsuarios);
+        break;
 
+        case 2:
+            break;
 
+        case 3:
+            break;
+
+        case 4:
+            /*for(i=0; i<CantUsuarios;i++)///mostrar solo los que tengan informacion.
+            {
+                if(listadoDeUsuario[i].idUsuario!=0)
+                {
+                     eUSo_mostrarLista(listadoDeUsuario,CantUsuarios);
+                }
+            }*/
+        eUSo_mostrarLista(listadoDeUsuario,CantUsuarios);
+        break;
+
+        case 5:
+            seguir='n';
+            break;
+        default:
+            printf("opcion incorrecta");
+            break;
+        }
+
+    }
+    /*eUso_alta(listadoDeUsuario,CantUsuarios);
+
+    eUso_alta(listadoDeUsuario,CantUsuarios);
+
+    eUSo_mostrarLista(listadoDeUsuario,CantUsuarios);*/
     return 0;
 }
